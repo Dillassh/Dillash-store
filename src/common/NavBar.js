462865670c51d8bef06a9613b62bf27dd2b631c4
Dillassh <dill.assh@gmail.com>
2022-05-25 16:29:47 +0300

    create new logo, find font

--- src/common/NavBar.js
+++ src/common/NavBar.js
@@ -1,25 +1,27 @@
-import logoDilla from "./logo-dillash.png";
+import logoDilla from "./logoD.PNG";
 import "./NavBar.css";
 
 function NavBar() {
 	return (
 		<div className='topNav'>
 			<div className='left-menu'>
-				<a className='nav_link' href='./'>
+				<a className='menu_links nav_link' href='./'>
 					Home{" "}
 				</a>
-				<a className='nav_link' href='./products'>
+				<a className='menu_links nav_link' href='./products'>
 					Products
 				</a>
 			</div>
 
-			<a className='col-sm-5' href='./'>
-				<img src={logoDilla} alt='Logo' />
+			<a className='wish menu_links nav_link' href='./wishlist'>
+				Wishlist<span className='pill badge badge-pill bg-danger '>0</span>
 			</a>
 
-			<a className='wish nav_link' href='./wishlist'>
-				Wishlist<span className='badge badge-pill bg-warning'>0</span>
-			</a>
+			<div className='logo_positioning'>
+				<a href='./'>
+					<img src={logoDilla} alt='Logo' />
+				</a>
+			</div>
 		</div>
 	);
 }
